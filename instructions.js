d57b6d89197b6b4a5048cd40590ec3f253c1aa42
kmh67calvin <122374860+kmh67calvin@users.noreply.github.com>
2023-04-09 13:46:26 -0400

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -1,3 +1,5 @@
+document.title = "wikipedia.org/wiki/Turin"
+
 toggleDarkMode = () => {
     document.body.classList.toggle("darkMode");
     document.querySelector(".box").classList.toggle("boxDarkMode");
